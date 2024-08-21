e0e3c3ff14b8aae32ef68eb74466110ea0a71527
atanasov2002 <atanasovatanas2002@gmail.com>
2024-08-21 21:42:22 +0200

    Fixing ReCaptcha

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,5 @@ from app import create_app
 app, mail, socketio, csrf = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app)
+    socketio.run(app, debug=True)
     # app[0].run(debug=True)
